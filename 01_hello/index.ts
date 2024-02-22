65c7cce247a89eb40cd65f046cb4a3fe1f3f81e6
Muhammad imran khan <81646567+imran752310@users.noreply.github.com>
2024-02-22 10:49:56 +0500

    Update index.ts

--- 01_hello/index.ts
+++ 01_hello/index.ts
@@ -1,4 +1,5 @@
 console.log("Hello I am Imran");
+
 const accountId:number =123456;
 let accountEmail:string = "imran@gmail.com";
 var accountPassword:string="imran12345";
@@ -8,5 +9,5 @@ console.log(accountId);
 console.log(accountEmail);
 console.log(accountPassword);
 console.log(accountCity);
-
+//print as a table
 console.table([accountId,accountEmail,accountPassword,accountCity]);
