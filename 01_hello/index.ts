2abfc8243ec731adedf3f64f2dd581086adc624c
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-02-22 10:48:45 +0500

    variables

--- 01_hello/index.ts
+++ 01_hello/index.ts
@@ -1,2 +1,12 @@
-var name = "Hello I am Imran Khan";
-console.log(name);
+console.log("Hello I am Imran");
+const accountId:number =123456;
+let accountEmail:string = "imran@gmail.com";
+var accountPassword:string="imran12345";
+let accountCity:string="Islamabad";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
