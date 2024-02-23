37d1f7e41feca878956ded1062a7559f5958807f
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-02-23 23:25:29 +0500

    ok

---
+++ 06_union_literals/index.ts
@@ -0,0 +1,6 @@
+// Union Titerals 
+// 1. Write a union type that represents the set of all possible values for an HTML element's id attribute. 
+type Id = string | number;
+
+let myId: Id = "myElement";
+console.log(myId); // Output: myElement
